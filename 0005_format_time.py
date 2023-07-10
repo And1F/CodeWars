e700f10e914647de7f52afdb5186f759f9adbeb0
And1F <123935502+And1F@users.noreply.github.com>
2023-07-10 21:56:14 +0200

    Update 0005_format_time.py

--- 0005_format_time.py
+++ 0005_format_time.py
@@ -1,6 +1,8 @@
 def format_duration(seconds):
-    times = {"decade":311040000, "year":31104000, "month":2592000, "day":86400, "hour":3600,  "minute":60, "second":1}
+    times = {"year":31536000, "day":86400, "hour":3600,  "minute":60, "second":1}
     string = []
+    if seconds < 1:
+        return "now"
     for time in times.items():
         minus = seconds//time[1]
         seconds -= minus*time[1]
@@ -10,5 +12,4 @@ def format_duration(seconds):
             string.append(f"{minus} {time[0]}s")
     return " and".join(", ".join(string).rsplit(",", 1))
 
-
-print(format_duration(3662))  
+print(format_duration(33243586))  
