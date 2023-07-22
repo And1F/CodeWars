22bc4a75bb3a760ee407749c92251b83e2301d5e
And1F <123935502+And1F@users.noreply.github.com>
2023-07-22 23:54:17 +0200

    Add files via upload

---
+++ 0017_cake.py
@@ -0,0 +1,8 @@
+def cakes(recipe, available):
+    a = []
+    try:
+        for ingredient in list(recipe.keys()):
+            a.append(available[ingredient]/recipe[ingredient])
+        return int(min(a)//1)
+    except:
+        return 0
