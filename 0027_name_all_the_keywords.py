c47e1f64c38cdb5208222704610309cc96cd5519
And1F <123935502+And1F@users.noreply.github.com>
2023-08-02 08:59:44 +0200

    Add files via upload

---
+++ 0027_name_all_the_keywords.py
@@ -0,0 +1,6 @@
+keywords = {"if", "elif", "else", "for", "and", "or","try", "except", 
+            "pass", "continue", "break", "raise", "import", "from", 
+            "as", "is", "in", "not", "with", "assert", "while", "True", 
+            "False", "def", "nonlocal", "global", "return","async", 
+            "await", "del", "finally", "lambda", "yield", "class", "None",} # "case" "match" 
+      
