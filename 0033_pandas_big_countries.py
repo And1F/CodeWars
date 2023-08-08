3d5f1da7f2df6ecd02380c67609ba5aba6da348a
And1F <123935502+And1F@users.noreply.github.com>
2023-08-08 17:29:08 +0200

    Add files via upload

---
+++ 0033_pandas_big_countries.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    ans = world[(world["population"]>25000000) | (world["area"]>3000000)]
+    return ans[["name","population", "area"]]
