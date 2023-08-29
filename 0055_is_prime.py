895b3cff26419357f645296dd4d721319bae5d85
Andreas Frehner <123935502+And1F@users.noreply.github.com>
2023-08-29 12:05:33 +0200

    Add files via upload

---
+++ 0055_is_prime.py
@@ -0,0 +1,10 @@
+def is_prime(num):
+    if num <= 1: return False
+    elif num <= 3:return True
+    elif num % 2 == 0 or num % 3 == 0:return False
+    i = 5
+    while i**2 <= num:
+        if num % i == 0 or num % (i + 2) == 0:return False
+        i += 6
+    return True
+print(is_prime(2))
